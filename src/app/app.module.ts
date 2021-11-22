de0600a09a94e10d400646512622dad1713e2709
v.shchura <v.shchura@itransition.com>
2021-11-22 12:10:57 +0300

    Made a navbar, set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StatsComponent } from './stats/stats.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'stats', component: StatsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', component: NotFoundComponent},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatsComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
