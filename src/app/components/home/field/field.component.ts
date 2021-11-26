5b3aa8bc3128378006899782ff941f674559a9c3
v.shchura <v.shchura@itransition.com>
2021-11-26 12:04:10 +0300

    Deleted size inputs

--- src/app/components/home/field/field.component.ts
+++ src/app/components/home/field/field.component.ts
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./field.component.sass']
 })
 export class FieldComponent implements OnInit {
-  rows: number = 8
-  columns: number = 15
-
-  tr = new Array(this.rows)
-  td = new Array(this.columns)
+  tr = new Array(10)
+  td = new Array(15)
 
 
   constructor() { }
@@ -18,33 +15,5 @@ export class FieldComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  setFieldSize() {
-    if (this.rows <= 8 && this.rows >= 5)
-      this.tr.length = this.rows
-    else if (this.rows > 8) {
-      this.rows = 8
-      this.tr.length = this.rows
-      alert('Max. value of rows: 8')
-    }
-    else {
-      this.rows = 5
-      this.tr.length = this.rows
-      alert('Min. value of rows: 5')
-    }
-
-    if (this.columns <= 15 && this.columns >= 10)
-      this.td.length = this.columns
-    else if (this.columns > 15) {
-      this.columns = 15
-      this.td.length = this.columns
-      alert('Max. value of columns: 15')
-    }
-    else {
-      this.columns = 10
-      this.td.length = this.columns
-      alert('Min. value of columns: 10')
-    }
-
-  }
 
 }
