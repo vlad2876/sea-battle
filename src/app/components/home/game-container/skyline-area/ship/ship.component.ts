53624b76c6c6694791862dc3089bd61f663a82f1
v.shchura <v.shchura@itransition.com>
2021-12-03 09:36:17 +0300

    Code refactoring

--- src/app/components/home/game-container/skyline-area/ship/ship.component.ts
+++ src/app/components/home/game-container/skyline-area/ship/ship.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'home-ship',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ship.component.sass']
 })
 export class ShipComponent implements OnInit {
+  @Input() bigShipSelected: boolean = false;
+  @Input() smallShipSelected: boolean = false;
 
   constructor() { }
 
