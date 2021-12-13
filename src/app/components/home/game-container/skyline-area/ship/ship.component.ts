bb9319d247167e850da83503f1f892062cdb873d
v.shchura <v.shchura@itransition.com>
2021-12-14 01:38:03 +0300

    Remove service, positioning moved to a higher level, animation changes

--- src/app/components/home/game-container/skyline-area/ship/ship.component.ts
+++ src/app/components/home/game-container/skyline-area/ship/ship.component.ts
@@ -1,28 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ShipType} from "../../game-container-enums/ship-type.enum";
-import {animate, state, style, transition, trigger} from "@angular/animations";
-import {ShipAnimationService} from "./ship-animation.service";
 
 @Component({
   selector: 'home-ship',
   templateUrl: './ship.component.html',
-  styleUrls: ['./ship.component.sass'],
-  providers: [ShipAnimationService],
-  animations: [
-    trigger('shipMovement', [
-      state('start', style({position: 'relative', left: 0})),
-      state('end', style({position: 'relative', left: 900})),
-      state('void', style({opacity: 0})),
-      transition('start => end', animate('3s')),
-      transition('void => start', [
-        style({ opacity: 0, position: 'relative', left: -150 }),
-        animate('1s', style({ opacity: 1, position: 'relative', left: 0 })),
-      ]),
-      transition('end => void', [
-        animate('1s', style({ opacity: 0, position: 'relative', left: 1050 })),
-      ]),
-    ])
-  ]
+  styleUrls: ['./ship.component.sass']
+
 })
 export class ShipComponent implements OnInit {
 
@@ -35,7 +18,7 @@ export class ShipComponent implements OnInit {
 
   @Input() shipType: ShipType = ShipType.BigShip;
 
-  constructor(public shipAnimationService: ShipAnimationService) { }
+  constructor() { }
 
   ngOnInit() {
   }
--- src/app/components/home/game-container/skyline-area/skyline-area.component.ts
+++ src/app/components/home/game-container/skyline-area/skyline-area.component.ts
@@ -1,14 +1,31 @@
 import {Component, OnInit} from '@angular/core';
 import {ShipType} from "../game-container-enums/ship-type.enum";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'home-skyline-area',
   templateUrl: './skyline-area.component.html',
-  styleUrls: ['./skyline-area.component.sass']
+  styleUrls: ['./skyline-area.component.sass'],
+  animations: [
+    trigger('shipMovement', [
+      state('start', style({left: -210})),
+      state('end', style({left: 1050})),
+      transition('start => end', animate('3s'))
+    ])
+  ]
 })
 export class SkylineAreaComponent implements OnInit {
 
   ShipType = ShipType;
+
+  state = 'start';
+
+  move = setInterval(() => {
+    this.state = 'end'
+    setTimeout(() => {
+      this.state = 'start'
+    }, 3100)
+  }, 3200)
 
   constructor() {
   }
