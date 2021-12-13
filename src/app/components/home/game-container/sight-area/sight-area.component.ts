021fbcd5ac0c702f4d9bf417bd68d3ae8b122070
v.shchura <v.shchura@itransition.com>
2021-12-13 23:37:50 +0300

    Removed the service, replaced numbers with data, replaced ngStyle

--- src/app/components/home/game-container/sight-area/sight-area.component.ts
+++ src/app/components/home/game-container/sight-area/sight-area.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SightControlKey} from "../game-container-enums/sight-control-key.enum";
 
 @Component({
   selector: 'home-sight-area',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sight-area.component.sass']
 })
 export class SightAreaComponent implements OnInit {
+  sightStep = 7;
+  width: number;
+  leftIndent: number;
+  sightElement: HTMLElement;
 
-  constructor() { }
+  onKeydown(event: { keyCode: SightControlKey }) {
+    if (event.keyCode === SightControlKey.ArrowRight && this.leftIndent < this.width) {
+      this.sightElement.style.left = this.leftIndent + this.sightStep + 'px';
+      this.leftIndent += this.sightStep;
+    }
 
-  ngOnInit() {
+    if (event.keyCode === SightControlKey.ArrowLeft && this.leftIndent > 0) {
+      this.sightElement.style.left = this.leftIndent - this.sightStep + 'px';
+      this.leftIndent -= this.sightStep;
+    }
   }
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    const sightAreaElement: HTMLElement = document.querySelector('#sight-area');
+    this.sightElement = document.querySelector('.sight-area__sight');
+    this.width = sightAreaElement.offsetWidth - this.sightElement.offsetWidth;
+    this.leftIndent = this.width / 2;
+    this.sightElement.style.left = this.leftIndent + 'px';
+  }
 }
--- src/app/components/home/game-container/sight-area/sight/sight.component.ts
+++ src/app/components/home/game-container/sight-area/sight/sight.component.ts
@@ -1,17 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {SightControlService} from "./sight-control.service";
-import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'home-sight',
   templateUrl: './sight.component.html',
-  styleUrls: ['./sight.component.sass'],
-  providers: [SightControlService],
+  styleUrls: ['./sight.component.sass']
 })
 
 export class SightComponent implements OnInit {
 
-  constructor(public sightControlService: SightControlService) {
+  constructor() {
   }
 
   ngOnInit() {
