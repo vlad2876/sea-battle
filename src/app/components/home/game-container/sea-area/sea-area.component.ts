b6d80cfccac5025773dec47e298f0b5a1a5b370d
v.shchura <v.shchura@itransition.com>
2021-12-14 01:42:51 +0300

    Removed service

--- src/app/components/home/game-container/sea-area/sea-area.component.ts
+++ src/app/components/home/game-container/sea-area/sea-area.component.ts
@@ -1,22 +1,41 @@
 import {Component, OnInit} from '@angular/core';
-import {ShotAnimationService} from "./shot-animation.service";
 
 @Component({
   selector: 'home-sea-area',
   templateUrl: './sea-area.component.html',
-  styleUrls: ['./sea-area.component.sass'],
-  providers: [ShotAnimationService],
+  styleUrls: ['./sea-area.component.sass']
 })
 export class SeaAreaComponent implements OnInit {
+  rowsCount = 10;
+  columnsCount = 15;
 
-  constructor(public shotAnimationService: ShotAnimationService) {
+  seaAreaCells: SeaAreaCell[][] = [];
+
+  readonly activeColumnIndex: number = 7;
+
+  shotTimeout = 1;
+
+  shot = setInterval(() => {
+    for (let i = 9; i >= 0; i--){
+      this.shotTimeout++;
+      setTimeout(() => {
+        this.seaAreaCells[i][this.activeColumnIndex].active = true;
+        setTimeout(() => {
+          this.seaAreaCells[i][this.activeColumnIndex].active = false;
+        }, 100);
+      }, 100 * this.shotTimeout);
+    }
+    this.shotTimeout = 1;
+  }, 2000)
+
+  constructor() {
   }
 
   ngOnInit() {
-    for (let i = 0; i < this.shotAnimationService.rowsCount; i++) {
-      this.shotAnimationService.seaAreaCells.push([]);
-      for (let j = 0; j < this.shotAnimationService.columnsCount; j++) {
-        this.shotAnimationService.seaAreaCells[i][j] = new SeaAreaCell(false);
+    for (let i = 0; i < this.rowsCount; i++) {
+      this.seaAreaCells.push([]);
+      for (let j = 0; j < this.columnsCount; j++) {
+        this.seaAreaCells[i][j] = new SeaAreaCell(false);
       }
     }
   }
