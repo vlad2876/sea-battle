24317c1d2afad60fe4041b0c74f68d429d87a466
v.shchura <v.shchura@itransition.com>
2021-12-22 01:36:24 +0300

    Added new methods

--- src/app/entities/SeaBattleGame.ts
+++ src/app/entities/SeaBattleGame.ts
@@ -3,59 +3,54 @@ import {StatusType} from "../gameplay-enums/status-type.enum";
 import {interval, map, take} from "rxjs";
 import {GameDuration} from "../gameplay-enums/game-duration.enum";
 import {PointsCountPerHit} from "../gameplay-enums/points-count-per-hit.enum";
+import {ShipType} from "../components/home/game-container/game-container-enums/ship-type.enum";
 
 export class SeaBattleGame {
 
   public countdownTime: number = this.maxGameTime;
   public maxShells = 10;
-  public shellsRemaining = 10;
+  public shellsRemaining = this.maxShells;
 
   constructor(
-    public username: string,
-    public status: StatusType,
-    public points: number = 0,
-    public maxGameTime: GameDuration,
-    public duration: number,
-    public shotsCount: number,
-    public bigShipsDowned: number = 0,
-    public smallShipsDowned: number = 0,
-    public startDate: number,
-    public endDate: number,
-    public areaWidth: number,
-    public areaHeight: number,
-    public gameSpeed: SpeedType
+    private username: string,
+    private status: StatusType,
+    private points: number = 0,
+    private maxGameTime: GameDuration,
+    private duration: number,
+    private shotsCount: number,
+    private bigShipsDowned: number = 0,
+    private smallShipsDowned: number = 0,
+    private startDate: number,
+    private endDate: number,
+    private areaWidth: number,
+    private areaHeight: number,
+    private gameSpeed: SpeedType
   ) {
-  }
-
-  public getData() {
-    return new SeaBattleGame(
-      this.username,
-      this.status,
-      this.points,
-      this.maxGameTime,
-      this.duration,
-      this.shotsCount,
-      this.bigShipsDowned,
-      this.smallShipsDowned,
-      this.startDate,
-      this.endDate,
-      this.areaWidth,
-      this.areaHeight,
-      this.gameSpeed
-    );
   }
 
   private bigShipOnHit() {
     this.points += PointsCountPerHit.HitBigShip;
+    this.bigShipsDowned++;
   }
 
   private smallShipOnHit() {
     this.points += PointsCountPerHit.HitSmallShip;
+    this.smallShipsDowned++;
   }
 
-  private missShot() {
+  public missShot() {
     if (this.points >= PointsCountPerHit.MissShot) {
       this.points -= PointsCountPerHit.MissShot;
+    }
+  }
+
+  public onHit(shipType: ShipType) {
+    if (shipType === ShipType.BigShip) {
+      this.bigShipOnHit();
+    }
+
+    if (shipType === ShipType.SmallShip) {
+      this.smallShipOnHit();
     }
   }
 
@@ -72,22 +67,30 @@ export class SeaBattleGame {
       map(sec => this.maxGameTime - sec)
     ).subscribe(value => {
       this.countdownTime = value;
-      if (this.countdownTime === 0){
+      if (this.countdownTime === 0) {
         this.gameOver();
       }
     });
   }
 
-  public startGame() {
+  private releaseNewShip() {
   }
 
-  private pauseGame() {
+  public startGame() {
+    this.startDate = Date.now();
+  }
+
+  public newGame() {
+  }
+
+  public pauseGame() {
     this.duration = this.maxGameTime - this.countdownTime;
   }
 
-  private resumeGame() {
+  public resumeGame() {
   }
 
   private gameOver() {
+    this.endDate = Date.now();
   }
 }
