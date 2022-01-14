4e4162ab14a124cf05d03d248117fa389b2c6f24
v.shchura <v.shchura@itransition.com>
2022-01-14 15:28:08 +0300

    Changed the way the isDisabled property is initialized

--- src/app/entities/SeaBattleSight.ts
+++ src/app/entities/SeaBattleSight.ts
@@ -3,6 +3,6 @@ import {SightData} from "./SightData";
 export class SeaBattleSight {
   constructor(private sightData: SightData) {
     sightData.leftIndent = sightData.leftIndent ? sightData.leftIndent : 0;
-    sightData.isDisabled = sightData.isDisabled ? sightData.isDisabled : false;
+    sightData.isDisabled = Boolean(sightData.isDisabled);
   }
 }
